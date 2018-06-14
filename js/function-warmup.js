c5eb4b99d6a867e8ae85c7c7d723ea95c501c31b
Amos Davis <amosdavis@Amoss-MacBook-Pro.local>
2018-06-14 10:12:14 -0500

    first warmup function exercise

--- js/function-warmup.js
+++ js/function-warmup.js
@@ -3,8 +3,10 @@
 
 
     function makeUsername (firstName, age) {
-        return firstName.length +
+        return firstName.length + firstName.toLowerCase().substring(0,1) + age.valueOf()
     }
+
+    console.log(makeUsername("Jack", 12));
 
 
 
