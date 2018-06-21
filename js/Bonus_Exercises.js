652bb38f92b77268184cc4344b3f01ec40418331
Amos Davis <amosdavis@Amoss-MacBook-Pro.local>
2018-06-21 11:06:38 -0500

    Bonus Object Exercises

---
+++ js/Bonus_Exercises.js
@@ -0,0 +1,42 @@
+"use strict";
+
+(function (){
+
+
+
+function toggle(string) {
+    if (string) {
+        console.log(string.split(""));
+
+        var
+
+        for string.split() {
+            if (string.split("") === string.split("").toUpperCase()) {
+                console.log(string.split("").toLowerCase());
+            }
+        }
+    }
+
+
+}
+
+ console.log(toggle('Hello'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
