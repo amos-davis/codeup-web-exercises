c1b665f2f45ff44cf2e338b9c3d9c00fa7a34762
Amos Davis <amosdavis@amoss-mbp.attlocal.net>
2018-07-01 15:36:56 -0500

    jQuery Intro Exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,22 @@
-$('h1').click(function() {
-    $(this).css('background-color', 'yellow');
-})
-//
-$('p').dblclick(function() {
-    $(this).css('font-size', '24px');
-})
+"use script";
 
-$('li').hover(function() {
-        $(this).css('background-color', 'red')
-    },
-    function () {
-        $(this).css('background-color', 'black')
-    })
+$(document).ready(function() {
+
+    alert('The DOM has finished loading.')
+
+});
+
+// $('h1').click(function() {
+//     $(this).css('background-color', 'yellow');
+// })
+// //
+// $('p').dblclick(function() {
+//     $(this).css('font-size', '24px');
+// })
+//
+// $('li').hover(function() {
+//         $(this).css('background-color', 'red')
+//     },
+//     function () {
+//         $(this).css('background-color', 'black')
+//     })
