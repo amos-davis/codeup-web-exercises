e111cc4b70376df9f6cccc51b02973c583ffe1f7
Amos Davis <amosdavis@Amoss-MacBook-Pro.local>
2018-07-03 15:16:41 -0500

    Effects with 1 bonus part completed

--- js/jqueryAttributes.js
+++ js/jqueryAttributes.js
@@ -1,11 +1,6 @@
 "use strict";
 $(function () {
 
-
-
-
-
-
     $('#btn-park').click(function () {
         $('ul').each(function () {
             $(this).children().last().toggleClass('btn-flop');
@@ -18,7 +13,44 @@ $(function () {
 
     $('li').click(function () {
         $(this).parent().children().first().css('background-color', 'blue');
-    })
+    });
+
+$('span').click(function() {
+    $('#Effects-div').hide();
+});
+
+$('#national-parks').hide();
+$('#state-parks-texas').hide();
+$('#national-parks-wildlife').hide();
+
+$('#national-parks-heading').click(function () {
+    $('#national-parks').slideToggle();
+})
+$('#state-parks-heading').click(function () {
+    $('#state-parks-texas').slideToggle();
+})
+$('#wildlife-heading').click(function () {
+    $('#national-parks-wildlife').slideToggle();
+})
+   // ------- more concise way of hiding h3s------
+    // var h3s = $('.parks');
+    // h3s.click(function () {
+    //     $(this).next().fadeIn();
+    // })
+
+$('h2').hide().delay(8000).fadeIn(3000);
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
