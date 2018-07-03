a51587e4ab2b542c4659c2063387cbb5813fed46
Amos Davis <amosdavis@Amoss-MacBook-Pro.local>
2018-07-03 11:28:00 -0500

    Traversal Method Exercise

--- js/jqueryAttributes.js
+++ js/jqueryAttributes.js
@@ -3,14 +3,24 @@ $(function () {
 
 
 
-$('#details').click(function () {
-    event.preventDefault();
-    $('dd').toggleClass('invisible');
+
+
+
+    $('#btn-park').click(function () {
+        $('ul').each(function () {
+            $(this).children().last().toggleClass('btn-flop');
+        })
+    });
+
+    $('h3').click(function () {
+        $(this).next().children().css('font-weight', 'bold');
+    });
+
+    $('li').click(function () {
+        $(this).parent().children().first().css('background-color', 'blue');
+    })
 });
 
-$('dt').click(function () {
-    $(this).toggleClass('highlighted');
-});
 
 
 
@@ -18,11 +28,6 @@ $('dt').click(function () {
 
 
 
-
-
-
-
-});
 
 
 
