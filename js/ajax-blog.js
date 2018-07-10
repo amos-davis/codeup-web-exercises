5b27e08a11aa13538db27cb5d9aa3725cc5b18d1
Amos Davis <amos.davis367@gmail.com>
2018-07-10 11:02:18 -0500

    blog still working on it

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -13,12 +13,15 @@ $.ajax({
         listItem += "<h3>" + objItem.title + "</h3>";
         listItem += "</div>";
         listItem += "<p>(" + objItem.date + ")</p>";
-        listItem += "<p><em>" + "Let'\s talk a little about " + objItem.categories + "..." + "</em></em></p>";
+        listItem += "<p><em>" + "Let'\s talk a little about " + objItem.categories + "..." + "</em></p>";
+        listItem += "<div>" + objItem.picture + "</div>"
+        // var listItem = '';
+        // listItem += '<img class="greekGod" src="' + objItem.picture1 + '">';
+        // listItem += '<img class="sweets" src="' + objItem.picture2 + '">';
+        $('#posts').append(listItem);
 
-        $('.title-post1').append(listItem);
     });
 
-    $(function (data) {
 
-    });
 });
+
